38bc0c4df2e7cf49bbec4b4acf8dd85f47592b89
sage <laymonage@gmail.com>
2020-11-27 23:13:06 +0700

    project: Add Tailwind CSS 2.0
    We need to use PostCSS 7 compatibility build until Rollup supports
    PostCSS 8.

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,8 @@
 <template>
   <h1>{{ msg }}</h1>
-  <button @click="count++">count is: {{ count }}</button>
+  <button class="p-2 bg-blue-700 text-white font-bold rounded" @click="count++">
+    count is: {{ count }}
+  </button>
   <p>
     Edit <code>components/HelloWorld.vue</code> to test hot module replacement.
   </p>
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  purge: ['./src/**/*.vue', './src/**/*.ts'],
+  darkMode: 'class',
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+};
