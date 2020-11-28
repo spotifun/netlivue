61a7527fe366d7bd600f6ef65d01a5c6aa0f560f
sage <laymonage@gmail.com>
2020-11-29 02:37:36 +0700

    router: Add vue-router

---
+++ src/router.ts
@@ -0,0 +1,11 @@
+import { createWebHistory, createRouter } from "vue-router";
+import Home from "./views/Home.vue";
+
+const history = createWebHistory();
+
+const routes = [
+  { path: "/", component: Home },
+];
+
+const router = createRouter({ history, routes });
+export default router;
