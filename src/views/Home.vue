8a128c58b9b1e31ae82a419741afc29b44c14219
sage <laymonage@gmail.com>
2020-11-29 02:36:50 +0700

    views: Create Home view

---
+++ src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <BaseContainer>
+    <img class="mx-auto" alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Hello Vue 3.0 + Vite" />
+  </BaseContainer>
+</template>
+
+<script>
+import BaseContainer from '../components/base/BaseContainer.vue';
+import HelloWorld from '../components/HelloWorld.vue';
+
+export default {
+  name: 'Home',
+  components: {
+    BaseContainer,
+    HelloWorld,
+  },
+};
+</script>
