a74ff4692151be5cc41609c64f8263f06256e9dc
sage <laymonage@gmail.com>
2020-12-03 10:29:14 +0700

    views: Replace HelloWorld with LoginButton in Home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,19 @@
 <template>
   <BaseContainer>
     <img class="mx-auto" alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Hello Vue 3.0 + Vite" />
+    <LoginButton />
   </BaseContainer>
 </template>
 
 <script>
 import BaseContainer from '../components/base/BaseContainer.vue';
-import HelloWorld from '../components/HelloWorld.vue';
+import LoginButton from '../components/LoginButton.vue';
 
 export default {
   name: 'Home',
   components: {
     BaseContainer,
-    HelloWorld,
+    LoginButton,
   },
 };
 </script>
