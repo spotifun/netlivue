2dee17df14d75354855672095838f03a53dd668c
sage <laymonage@gmail.com>
2020-12-03 10:31:08 +0700

    router: Add Auth view to router

--- src/router.ts
+++ src/router.ts
@@ -1,10 +1,12 @@
-import { createWebHistory, createRouter } from "vue-router";
-import Home from "./views/Home.vue";
+import { createWebHistory, createRouter } from 'vue-router';
+import Auth from './views/Auth.vue';
+import Home from './views/Home.vue';
 
 const history = createWebHistory();
 
 const routes = [
-  { path: "/", component: Home },
+  { path: '/', component: Home, name: 'Home' },
+  { path: '/auth', component: Auth, name: 'Profile' },
 ];
 
 const router = createRouter({ history, routes });
