c4bc3061dc35c4eb8eafc20231faf3edea7c9073
sage <laymonage@gmail.com>
2020-12-03 10:30:45 +0700

    views: Create Auth view

---
+++ src/views/Auth.vue
@@ -0,0 +1,18 @@
+<template>
+  <BaseContainer>
+    <Profile />
+  </BaseContainer>
+</template>
+
+<script>
+import BaseContainer from '../components/base/BaseContainer.vue';
+import Profile from '../components/Profile.vue';
+
+export default {
+  name: 'Home',
+  components: {
+    BaseContainer,
+    Profile,
+  },
+};
+</script>
