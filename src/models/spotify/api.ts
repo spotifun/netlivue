c28683d6e1e4d085dd5eb139ccd41a036bc5353f
sage <laymonage@gmail.com>
2020-12-12 01:10:17 +0700

    models: Add more types

--- src/models/spotify/api.ts
+++ src/models/spotify/api.ts
@@ -1,17 +1,33 @@
+export interface Followers {
+  href?: string;
+  total: number;
+}
+
+export interface Image {
+  height?: number;
+  url: string;
+  width?: number;
+}
+
+export interface ExplicitContentSettings {
+  filter_enabled: boolean;
+  filter_locked: boolean;
+}
+
 export interface User {
   country: string;
   display_name?: string;
-  external_urls: object;
-  followers: object;
+  external_urls: Record<string, string>;
+  followers: Followers;
   href: string;
   id: string;
-  images: Array<object>;
+  images: Array<Image>;
   type: string;
   uri: string;
 }
 
 export interface PrivateUser extends User {
   email: string;
-  explicit_content: object;
+  explicit_content: ExplicitContentSettings;
   product: string;
 }
