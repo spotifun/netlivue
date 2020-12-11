5f7da1ca21551fb2d3b00a2ae0da69547e165c2a
sage <laymonage@gmail.com>
2020-12-12 02:16:34 +0700

    ProfileInfo: Use BaseCard and add spinner icon

--- src/components/ProfileInfo.vue
+++ src/components/ProfileInfo.vue
@@ -1,31 +1,42 @@
 <template>
-  <div v-if="user">
-    <img
-      v-if="user.images"
-      class="rounded-full"
-      :src="user.images[0].url"
-      :alt="user.display_name"
+  <BaseCard>
+    <div
+      v-if="!user"
+      class="p-4"
     >
-    <h2 class="text-5xl font-bold p-2 m-2">
-      <a :href="user.external_urls.spotify">{{ user.display_name }}</a>
-    </h2>
-    <table class="text-center">
-      <thead>
-        <tr>
-          <th>Country</th>
-          <th>URI</th>
-          <th>Followers</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>{{ user.country }}</td>
-          <td>{{ user.uri }}</td>
-          <td>{{ user.followers.total }}</td>
-        </tr>
-      </tbody>
-    </table>
-  </div>
+      <icon-spinner class="animate-spin w-24 h-24 block mx-auto"></icon-spinner>
+    </div>
+    <div
+      v-else
+      class="text-center"
+    >
+      <img
+        v-if="user.images"
+        class="rounded-full mx-auto w-48"
+        :src="user.images[0].url"
+        :alt="user.display_name"
+      >
+      <h2 class="text-5xl font-bold p-2 m-2">
+        <a :href="user.external_urls.spotify">{{ user.display_name }}</a>
+      </h2>
+      <table class="text-center mx-auto">
+        <thead>
+          <tr>
+            <th>Country</th>
+            <th>URI</th>
+            <th>Followers</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>{{ user.country }}</td>
+            <td>{{ user.uri }}</td>
+            <td>{{ user.followers.total }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </BaseCard>
 </template>
 
 <script lang="ts">
