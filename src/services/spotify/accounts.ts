0b608528ecd449e3e5733282d6b5fc95d320fcd0
sage <laymonage@gmail.com>
2020-12-17 13:48:45 +0700

    services: Use states from store

--- src/services/spotify/accounts.ts
+++ src/services/spotify/accounts.ts
@@ -6,9 +6,13 @@ import { nanoid } from 'nanoid';
 import { AccessToken, RedirectQueryParams } from '../../models/spotify/auth';
 import * as env from './constants';
 import { sha256, b64urlsafe } from '../../utils';
-
-const ACCOUNTS_HOST = 'https://accounts.spotify.com/';
-const ACCOUNTS_BASE_URL = ACCOUNTS_HOST + 'api/';
+import {
+  codeVerifier,
+  setAccessToken,
+  setCodeVerifier,
+  setState,
+  state as authState,
+} from '../../store/auth';
 
 export const genCodeVerifier = () => nanoid(64);
 
@@ -24,8 +28,8 @@ export const genAuthURI = async () => {
   const codeChallenge = await genCodeChallenge(codeVerifier);
   const state = genState();
 
-  localStorage.setItem('spotifyCodeVerifier', codeVerifier);
-  localStorage.setItem('spotifyState', state);
+  setCodeVerifier(codeVerifier);
+  setState(state);
 
   return _authURI(codeChallenge, state);
 };
@@ -40,10 +44,13 @@ export const _authURI = (codeChallenge: string, state: string) => {
     state: state,
     scope: env.SPOTIFY_SCOPE,
   });
-  return ACCOUNTS_HOST + 'authorize?' + params;
+  return env.ACCOUNTS_AUTHORIZE_URL + params;
 };
 
-export const getAccessToken = async (code: string, codeVerifier: string) => {
+export const requestAccessToken = async (
+  code: string,
+  codeVerifier: string,
+): Promise<AccessToken> => {
   const body = new URLSearchParams({
     client_id: env.SPOTIFY_CLIENT_ID,
     grant_type: 'authorization_code',
@@ -56,46 +63,40 @@ export const getAccessToken = async (code: string, codeVerifier: string) => {
   return response.data;
 };
 
-export const refreshAccessToken = async (refreshToken: string) => {
+export const refreshAccessToken = async (
+  refreshToken: string,
+): Promise<AccessToken> => {
   const body = new URLSearchParams({
-    grant_type:'refresh_token',
-    refresh_token:refreshToken,
-    client_id:env.SPOTIFY_CLIENT_ID
+    grant_type: 'refresh_token',
+    refresh_token: refreshToken,
+    client_id: env.SPOTIFY_CLIENT_ID,
   });
 
   const response = await accounts.post('token', body);
   return response.data;
-}
+};
 
 export const getParams = (url: URL): RedirectQueryParams => {
-  const code = url.searchParams.get('code');
-  const state = url.searchParams.get('state');
-  const error = url.searchParams.get('error');
+  const code = url.searchParams.get('code') || undefined;
+  const state = url.searchParams.get('state') || undefined;
+  const error = url.searchParams.get('error') || undefined;
   return { code, state, error };
 };
 
-export const saveAccessToken = (result: AccessToken) =>
-  localStorage.setItem('spotifyAuth', JSON.stringify(result));
-
-export const loadAccessToken = (): AccessToken =>
-  JSON.parse(localStorage.getItem('spotifyAuth'));
-
 export const authenticate = async (params: RedirectQueryParams) => {
   const { code, state, error } = params;
-  const codeVerifier = localStorage.getItem('spotifyCodeVerifier');
-  const savedState = localStorage.getItem('spotifyState');
+  const savedCodeVerifier = codeVerifier.value;
+  const savedState = authState.value;
 
-  if (error || state !== savedState) {
+  if (error || state !== savedState || !code) {
     return false;
   }
 
-  const result = await getAccessToken(code, codeVerifier);
-  saveAccessToken(result);
+  const result = await requestAccessToken(code, savedCodeVerifier);
+  setAccessToken(result);
   return true;
 };
 
 export const accounts = axios.create({
-  baseURL: ACCOUNTS_BASE_URL,
+  baseURL: env.ACCOUNTS_BASE_URL,
 });
-
-export default accounts;
--- src/services/spotify/api.ts
+++ src/services/spotify/api.ts
@@ -1,35 +1,55 @@
 import axios from 'axios';
 
-import { loadAccessToken, refreshAccessToken, saveAccessToken } from './accounts';
+import { refreshAccessToken } from './accounts';
 import { PrivateUser } from '../../models/spotify/api';
+import { accessToken, setAccessToken } from '../../store/auth';
+import { API_BASE_URL } from './constants';
 
 export const getUserInfo = async (): Promise<PrivateUser> => {
   const response = await api.get('me');
   return await response.data;
 };
 
-const api = axios.create({
-    baseURL: 'https://api.spotify.com/v1/',
-    headers: {
-      Authorization: `Bearer ${loadAccessToken().access_token}`,
-    }
+export const api = axios.create({
+  baseURL: API_BASE_URL,
 });
 
-api.interceptors.response.use(
-  response => (response),
-  async error => {
-    const originalRequest = error.config;
-    if (error.response.status === 401 && error.response.data.error.message == "The access token expired" && !originalRequest._retry) {
-      originalRequest._retry = true;
-      
-      const auth = loadAccessToken();
-      const newAccessToken = await refreshAccessToken(auth.refresh_token);
-      saveAccessToken(newAccessToken);
+api.interceptors.request.use(
+  (config) => {
+    if (accessToken.value) {
+      const token = accessToken.value.access_token;
+      config.headers['Authorization'] = `Bearer ${token}`;
+    } else {
+      delete config.headers['Authorization'];
+    }
+    return config;
+  },
+  (error) => Promise.reject(error),
+);
 
-      originalRequest.headers['Authorization'] = `Bearer ${newAccessToken.access_token}`
+api.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config;
+    if (
+      error.response.status === 401 &&
+      error.response.data.error.message == 'The access token expired' &&
+      !originalRequest._retry
+    ) {
+      originalRequest._retry = true;
+
+      if (!accessToken.value) {
+        return Promise.reject(error);
+      }
+
+      const refreshToken = accessToken.value.refresh_token;
+      const newAccessToken = await refreshAccessToken(refreshToken);
+      setAccessToken(newAccessToken);
+      const token = newAccessToken.access_token;
+
+      originalRequest.headers['Authorization'] = `Bearer ${token}`;
       return api(originalRequest);
     }
     return Promise.reject(error);
-})
-
-export default api;
+  },
+);
