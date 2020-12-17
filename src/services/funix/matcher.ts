710033217121a18e92c2f8a5e1718430640024fa
sage <laymonage@gmail.com>
2020-12-17 22:26:51 +0700

    funix: Fix missing .data in service definitions

--- src/services/funix/matcher.ts
+++ src/services/funix/matcher.ts
@@ -14,22 +14,22 @@ import {
 } from '../../models/funix/matcher';
 
 export const matcher = axios.create({
-  baseURL: FUNIX_BASE_URL,
+  baseURL: FUNIX_BASE_URL + 'matcher/',
 });
 
 export const generate = async (data: GenerateRequest) =>
-  (await matcher.post('generate', data)) as GenerateResponse;
+  (await matcher.post('generate', data)).data as GenerateResponse;
 
 export const match = async (data: MatchRequest) =>
-  (await matcher.post('match', data)) as MatchResponse;
+  (await matcher.post('match', data)).data as MatchResponse;
 
 export const status = async (data: StatusRequest) =>
-  (await matcher.get('status', { params: data })) as StatusResponse;
+  (await matcher.get('status', { params: data })).data as StatusResponse;
 
 export const matchingID = async (data: MatchingIDRequest) =>
-  (await matcher.get('matching_id', { params: data })) as MatchingIDResponse;
+  (await matcher.get('matching_id', { params: data }))
+    .data as MatchingIDResponse;
 
 export const recommendation = async (data: RecommendationRequest) =>
-  (await matcher.get('recommendation', {
-    params: data,
-  })) as RecommendationResponse;
+  (await matcher.get('recommendation', { params: data }))
+    .data as RecommendationResponse;
