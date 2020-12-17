c7e4ecbfc4b8b4c378e2ee93ca4b79a82831bec1
sage <laymonage@gmail.com>
2020-12-17 20:25:09 +0700

    models: Create funix request and response models

---
+++ src/models/funix/matcher.ts
@@ -0,0 +1,68 @@
+enum MatchStatus {
+  no_match,
+  too_few,
+  ok,
+  too_much,
+}
+
+enum ResponseStatus {
+  ok,
+  error,
+}
+
+interface GenericRequest {
+  user_id: string;
+}
+
+export interface GenerateRequest extends GenericRequest {
+  access_token: string;
+}
+
+export interface GenerateResponse {
+  errors?: Array<Record<string, any>>;
+  matching_id?: number;
+  status: 'ok' | 'error';
+  user_id: string;
+}
+
+export interface MatchRequest extends GenericRequest {
+  matching_id: number;
+  access_token: string;
+}
+
+export interface MatchResponse {
+  errors?: Array<Record<string, any>>;
+  matching_id?: number;
+  status: MatchStatus;
+  user_id: string;
+}
+
+export interface StatusRequest {
+  matching_id: string;
+}
+
+export interface StatusResponse {
+  errors?: Array<Record<string, any>>;
+  matching_id?: number;
+  status: MatchStatus | ResponseStatus;
+}
+
+export interface MatchingIDRequest extends GenericRequest {}
+
+export interface MatchingIDResponse {
+  errors?: Array<Record<string, any>>;
+  matching_id?: number;
+  status: ResponseStatus;
+}
+
+export interface RecommendationSeeds {
+  seed_artists: Array<string>;
+  seed_tracks: Array<string>;
+}
+
+export interface RecommendationRequest extends GenericRequest {}
+
+export interface RecommendationResponse {
+  status: ResponseStatus;
+  seeds: RecommendationSeeds;
+}
