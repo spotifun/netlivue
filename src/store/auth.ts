6ff7c37431a546e690b18dd37d95aac35b5adcde
sage <laymonage@gmail.com>
2020-12-17 17:01:08 +0700

    store/auth: Create loggedIn state

--- src/store/auth.ts
+++ src/store/auth.ts
@@ -1,10 +1,11 @@
-import { readonly, ref } from 'vue';
+import { readonly, ref, watch } from 'vue';
 import { createStore } from '.';
 import { AccessToken } from '../models/spotify/accounts';
 
 export const {
   state: accessToken,
   setState: setAccessToken,
+  deleteState: deleteAccessToken,
 } = createStore<AccessToken>('spotifyAuth');
 
 export const {
@@ -15,3 +16,7 @@ export const {
 export const { state: state, setState: setState } = createStore<String>(
   'spotifyState',
 );
+
+const _loggedIn = ref(!!accessToken.value);
+watch(accessToken, (val) => (_loggedIn.value = !!val));
+export const loggedIn = readonly(_loggedIn);
