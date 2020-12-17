31a378aade552126e7a6e9551fd4391e89e5f58e
sage <laymonage@gmail.com>
2020-12-17 17:00:36 +0700

    store: Provide deleteState function

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,18 @@
 import { readonly, ref } from 'vue';
 
 export const createStore = <T>(key: string) => {
+  const state = ref<T>();
   const stored = localStorage.getItem(key);
-  const state = ref(stored ? JSON.parse(stored) : undefined);
+  if (stored) {
+    state.value = JSON.parse(stored) as T;
+  }
   const setState = (val: T) => {
     state.value = val;
     localStorage.setItem(key, JSON.stringify(val));
   };
-  return { state: readonly(state), setState };
+  const deleteState = () => {
+    state.value = undefined;
+    delete localStorage[key];
+  };
+  return { state: readonly(state), setState, deleteState };
 };
