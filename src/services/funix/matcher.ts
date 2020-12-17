228096095bbb1d790e89a7a96b403048a71dce20
sage <laymonage@gmail.com>
2020-12-18 00:14:17 +0700

    matcher: Rename service functions for better readability

--- src/services/funix/matcher.ts
+++ src/services/funix/matcher.ts
@@ -17,19 +17,19 @@ export const matcher = axios.create({
   baseURL: FUNIX_BASE_URL + 'matcher/',
 });
 
-export const generate = async (data: GenerateRequest) =>
+export const createRoom = async (data: GenerateRequest) =>
   (await matcher.post('generate', data)).data as GenerateResponse;
 
-export const match = async (data: MatchRequest) =>
+export const joinRoom = async (data: MatchRequest) =>
   (await matcher.post('match', data)).data as MatchResponse;
 
-export const status = async (data: StatusRequest) =>
+export const getRoomStatus = async (data: StatusRequest) =>
   (await matcher.get('status', { params: data })).data as StatusResponse;
 
-export const matchingID = async (data: MatchingIDRequest) =>
+export const getRoomID = async (data: MatchingIDRequest) =>
   (await matcher.get('matching_id', { params: data }))
     .data as MatchingIDResponse;
 
-export const recommendation = async (data: RecommendationRequest) =>
+export const getRecommendation = async (data: RecommendationRequest) =>
   (await matcher.get('recommendation', { params: data }))
     .data as RecommendationResponse;
