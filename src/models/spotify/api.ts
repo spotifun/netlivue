30895de5c4cf855b0bbfe9be3904878899c640c6
sage <laymonage@gmail.com>
2020-12-19 23:07:38 +0700

    models/spotify: Add types for recommendation API

--- src/models/spotify/api.ts
+++ src/models/spotify/api.ts
@@ -31,3 +31,50 @@ export interface PrivateUser extends PublicUser {
   explicit_content: ExplicitContentSettings;
   product: string;
 }
+
+export interface Artist {
+  external_urls: Record<string, string>;
+  href: string;
+  id: string;
+  name: string;
+  type: string;
+  uri: string;
+}
+
+export interface Track {
+  artists: Array<Artist>;
+  disc_number: number;
+  duration_ms: number;
+  explicit: boolean;
+  external_urls: Record<string, string>;
+  href: string;
+  id: string;
+  is_playable: boolean;
+  name: string;
+  preview_url: string;
+  track_number: number;
+  type: string;
+  uri: string;
+}
+
+export interface RecommendationSeed {
+  initialPoolSize: number;
+  afterFilteringSize: number;
+  afterRelinkingSize: number;
+  href: string;
+  id: string;
+  type: string;
+}
+
+export interface RecommendationRequest {
+  limit?: number;
+  market?: string;
+  seed_artists: Array<string>;
+  seed_genres: Array<string>;
+  seed_tracks: Array<string>;
+}
+
+export interface RecommendationResponse {
+  tracks: Array<Track>;
+  seeds: Array<RecommendationSeed>;
+}
