b99aa3f71995c97804335436b878767347a4a30a
sage <laymonage@gmail.com>
2020-12-27 20:28:22 +0700

    Home: Fix title on smaller screens

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <BaseContainer>
     <BaseCard class="text-center">
-      <h1 class="text-8xl font-bold p-4">spotifun</h1>
+      <h1 class="text-6xl sm:text-8xl font-bold p-4">spotifun</h1>
       <AuthButton class="mt-8 mb-4" />
     </BaseCard>
   </BaseContainer>
