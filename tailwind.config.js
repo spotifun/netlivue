89658c2a5c4932342a56be62eb44cec9ef0fba24
sage <laymonage@gmail.com>
2020-12-28 11:49:26 +0700

    tailwind: Add xs and ph breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@ module.exports = {
   purge: ['./src/**/*.vue', './src/**/*.ts'],
   darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        xs: { max: '639px' },
+        ph: { max: '425px' },
+      },
+    },
   },
   variants: {},
   plugins: [],
