7bab748accbaadaa823546cd14bd414a9e2395ec
sage <laymonage@gmail.com>
2020-12-28 12:17:12 +0700

    Home: Add tagline

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,10 @@
   <BaseContainer>
     <BaseCard class="text-center">
       <h1 class="text-6xl sm:text-8xl font-bold p-4">spotifun</h1>
+      <h2 class="text-xl sm:text-2xl mt-4">
+        <strong>A music recommendation app</strong>
+        for pairs of music connoisseurs.
+      </h2>
       <AuthButton class="mt-8 mb-4" />
     </BaseCard>
   </BaseContainer>
