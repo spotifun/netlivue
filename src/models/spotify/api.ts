036d0d648f37f22871ac6dc653f34879f991276e
sage <laymonage@gmail.com>
2020-12-28 11:09:34 +0700

    models/spotify: Add AlbumSimplified model and add missing attributes to Track

--- src/models/spotify/api.ts
+++ src/models/spotify/api.ts
@@ -41,16 +41,37 @@ export interface Artist {
   uri: string;
 }
 
+export interface AlbumSimplified {
+  album_group?: string;
+  album_type: 'ALBUM' | 'SINGLE' | 'COMPILATION';
+  artists: Array<Artist>;
+  available_markets: Array<string>;
+  external_urls: Record<string, string>;
+  href: string;
+  id: string;
+  images: Array<Image>;
+  name: string;
+  release_date: string;
+  release_date_precision: 'year' | 'month' | 'day';
+  restriction?: Record<string, string>;
+  type: string;
+  uri: string;
+}
+
 export interface Track {
+  album: AlbumSimplified;
   artists: Array<Artist>;
   disc_number: number;
   duration_ms: number;
   explicit: boolean;
+  external_ids: Record<string, string>;
   external_urls: Record<string, string>;
   href: string;
   id: string;
+  is_local: boolean;
   is_playable: boolean;
   name: string;
+  popularity: number;
   preview_url: string;
   track_number: number;
   type: string;
