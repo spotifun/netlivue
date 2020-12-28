2d564206605864d0c13ed8276c00a5371543e573
sage <laymonage@gmail.com>
2020-12-28 11:10:12 +0700

    TrackList: Show album art

--- src/components/TrackList.vue
+++ src/components/TrackList.vue
@@ -11,14 +11,21 @@
           class="bg-gray-500 bg-opacity-10 hover:bg-opacity-20 focus:bg-opacity-20
             flex px-6 py-4 rounded-xl my-2 items-center justify-between"
         >
-          <div>
-            <h3 class="font-bold text-xl">{{ track.name }}</h3>
-            <p>{{ artists[track.id] }}</p>
+          <div class="flex items-center">
+            <img
+              v-if="imageURL(track.album.images)"
+              :src="imageURL(track.album.images)"
+              :alt="track.album.name"
+            >
+            <div class="ml-4">
+              <h3 class="font-bold text-xl">{{ track.name }}</h3>
+              <p>{{ artists[track.id] }}</p>
+            </div>
           </div>
           <div>
             <span
               v-if="track.explicit"
-              class="p-2 border-2 border-gray-500 rounded-md mx-4"
+              class="p-2 border-2 border-gray-500 rounded-md mx-4 text-sm"
             >EXPLICIT</span>
             <span>
               {{ millisToMinutesSeconds(track.duration_ms) }}
@@ -32,12 +39,20 @@
 
 <script lang="ts">
 import { defineComponent, PropType, computed } from 'vue';
-import { Track } from '../models/spotify/api';
+import { Image, Track } from '../models/spotify/api';
 
 const millisToMinutesSeconds = (millis: number) => {
   const minutes = Math.floor(millis / 60000);
-  const seconds = ((millis % 60000) / 1000).toFixed(0);
-  return minutes + ':' + (seconds < 10 ? '0' : '') + seconds;
+  const seconds = (millis % 60000) / 1000;
+  return minutes + ':' + (seconds < 10 ? '0' : '') + seconds.toFixed(0);
+};
+
+const imageURL = (images: Image[]) => {
+  if (!images) {
+    return null;
+  }
+  const image = images[images.length - 1];
+  return image.url;
 };
 
 export default defineComponent({
@@ -77,6 +92,7 @@ export default defineComponent({
     return {
       artists,
       millisToMinutesSeconds,
+      imageURL,
     };
   },
 });
